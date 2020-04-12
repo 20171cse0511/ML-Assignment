57d387390a3c9dcae838824f70d53d9913c2de1a
20171cse0511 <60208219+20171cse0511@users.noreply.github.com>
2020-04-12 10:24:17 +0530

    Update IMDB_KNN.py

--- IMDB_KNN.py
+++ IMDB_KNN.py
@@ -1,6 +1,6 @@
 import numpy as np 
 import pandas as pd 
-df=pd.read_csv("C:\\Users\\DELL\\Desktop\\IMDB\\i.csv")
+df=pd.read_csv("i.csv")
 
 from sklearn.neighbors import KNeighborsClassifier 
 from sklearn.model_selection import train_test_split 
